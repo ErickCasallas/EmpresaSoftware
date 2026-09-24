2e410eb6aa8aa8612bd23d7a496e021eab7dc545
SANTIAGO PAEZ <santiago.paezv@uqvirtual.edu.co>
2026-09-23 22:00:18 -0500

    CRUDCliente

--- src/main/java/co/edu/uniquindio/EmpresaSoftware/Desarrollador.java
+++ src/main/java/co/edu/uniquindio/EmpresaSoftware/Desarrollador.java
@@ -1,15 +1,15 @@
 package co.edu.uniquindio.EmpresaSoftware;
 
-public class Desarrolladores {
+public class Desarrollador {
     private String id;
-    private String equipoTrabajo;
+    private String name;
     private String nivel;
     private String cantidadProyectoSilmutaneo;
     private double tarifaDia;
 
-    public Desarrolladores(String id, String equipoTrabajo, String nivel, String cantidadProyectoSilmutaneo, double tarifaDia) {
+    public Desarrollador(String id,String name, String nivel, String cantidadProyectoSilmutaneo, double tarifaDia) {
+        this.name=name;
         this.id = id;
-        this.equipoTrabajo = equipoTrabajo;
         this.nivel = nivel;
         this.cantidadProyectoSilmutaneo = cantidadProyectoSilmutaneo;
         this.tarifaDia = tarifaDia;
@@ -18,40 +18,32 @@ public class Desarrolladores {
     public String getId() {
         return id;
     }
-
     public void setId(String id) {
         this.id = id;
     }
-
-    public String getEquipoTrabajo() {
-        return equipoTrabajo;
-    }
-
-    public void setEquipoTrabajo(String equipoTrabajo) {
-        this.equipoTrabajo = equipoTrabajo;
-    }
-
     public String getNivel() {
         return nivel;
     }
-
     public void setNivel(String nivel) {
         this.nivel = nivel;
     }
-
     public String getCantidadProyectoSilmutaneo() {
         return cantidadProyectoSilmutaneo;
     }
-
     public void setCantidadProyectoSilmutaneo(String cantidadProyectoSilmutaneo) {
         this.cantidadProyectoSilmutaneo = cantidadProyectoSilmutaneo;
     }
-
     public double getTarifaDia() {
         return tarifaDia;
     }
-
     public void setTarifaDia(double tarifaDia) {
         this.tarifaDia = tarifaDia;
+    }
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
     }
 }
--- src/main/java/co/edu/uniquindio/EmpresaSoftware/MainDesarrollador.java
+++ src/main/java/co/edu/uniquindio/EmpresaSoftware/MainDesarrollador.java
@@ -1,4 +1,69 @@
 package co.edu.uniquindio.EmpresaSoftware;
 
+import javax.swing.*;
+
 public class MainDesarrollador {
+    static void main() {
+        Desarrollador[] equipoTrabajo=new Desarrollador[100];
+        int cantidadDesarrolladores=0;
+        int option;
+        do {
+            option = Integer.parseInt(
+                    JOptionPane.showInputDialog(
+                            "Desarrolladores\n\n"
+                                    + "1. Contratar desarrollador\n"
+                                    + "2. Lista de desarrolladores\n"
+                                    + "3. Buscar perfil de desarrollador\n"
+                                    + "4. Actualizar datos de desarrollador\n"
+                                    + "5. Eliminar desarrollador\n"
+                                    + "0. Salir\n\n"
+                                    + "Seleccione una opción:"
+                    )
+            );
+            switch (option){
+                case 1:
+                    //cantidadDesarrolladores=registarDesarrollador(desarrolladores, cantidadDesarrolladores);
+                    break;
+
+                case 2:
+                    //listarClientes(desarrolladores, cantidadDesarrolladores);
+                    break;
+
+                case 3:
+                    //buscarDesarrollador(desarrolladores, cantidadDesarrolladores);
+                    break;
+
+                case 4:
+                    //actualizarDesarrollador(desarrolladores, cantidadDesarrolladores);
+                    break;
+
+                case 5:
+                    //cantidadDesarrolladores = eliminarDesarrollador(desarrolladores, cantidadDesarrolladores);
+                    break;
+
+                case 0:
+                    JOptionPane.showMessageDialog(null, "Programa finalizado.");
+                    break;
+
+                default:
+                    JOptionPane.showMessageDialog(null, "Opción inválida.");
+            }
+            }while (option!=0);
+        }
+        public static int registrarDesarrollador(Desarrollador[] equipoTrabajo,int cantidadDesarrolladores) {
+            String documento = JOptionPane.showInputDialog("Ingrese el documento de identidad");
+            //Verificar que no haya un mismo cliente
+            for (int i = 0; i < cantidadDesarrolladores; i++) {
+                if (equipoTrabajo[i].getId().equals(documento)) {
+                    JOptionPane.showMessageDialog(null,"Ya existe un desarrollador con ese documento");
+                    return cantidadDesarrolladores;
+                }
+            }
+            String id=JOptionPane.showInputDialog("Ingrese su codigo de ID: ");
+            String name=JOptionPane.showInputDialog("Ingrese su nombre: ");
+            String nivel=JOptionPane.showInputDialog("Ingrese ");
+            String cantidadProyectoSilmutaneo=JOptionPane.showInputDialog("Ingrese su codigo de ID: ");
+            String tarifaDia=JOptionPane.showInputDialog("Ingrese su codigo de ID: ");
+            return 1;
+        }
 }
